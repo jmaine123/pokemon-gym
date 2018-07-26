649078e42bb012dad81d724fad726b63b04e6e0c
Ibteda Sabur <akkount35@gmail.com>
2018-07-26 13:50:32 -0400

    added blur in effect on other trainers when focused on one trainer

--- library.js
+++ library.js
@@ -28,92 +28,114 @@ var infoGrid = document.getElementsByClassName('move');
 
 var trainerRed = document.getElementById('red');
 
-trainerRed.addEventListener('click',function() {
+trainerRed.addEventListener('click', function() {
 
-    loadPokemon('red', 'pikachu');
-    loadPokemon('red', 'mewtwo');
-    loadPokemon('red', 'rapidash');
+  loadPokemon('red', 'pikachu');
+  loadPokemon('red', 'mewtwo');
+  loadPokemon('red', 'rapidash');
 
-    pokemonOne.style.backgroundColor = '#D76475';
-    pokemonTwo.style.backgroundColor = '#D76475';
-    pokemonThree.style.backgroundColor = '#D76475';
+  pokemonOne.style.backgroundColor = '#D76475';
+  pokemonTwo.style.backgroundColor = '#D76475';
+  pokemonThree.style.backgroundColor = '#D76475';
 
-    trainerRed.classList.remove("blur");
-    infoGrid[0].classList.remove("hidden");
+  trainerRed.classList.remove("blur");
+  infoGrid[0].classList.remove("hidden");
+  trainerStephanie.classList.add("blur");
+  infoGrid[1].classList.add("hidden");
+  trainerAsh.classList.add("blur");
+  infoGrid[2].classList.add("hidden");
 
-    var pikachuBall = document.getElementById('pikachu-ball');
-    pikachuBall.addEventListener('click', function() {
-      pokemonOne.style.backgroundImage = "url('images/pikachu_lightning.gif')";
-      pokemonOne.style.backgroundSize = '100% 100%';
-    });
+  var pikachuBall = document.getElementById('pikachu-ball');
+  pikachuBall.addEventListener('click', function() {
+    pokemonOne.style.backgroundImage = "url('images/pikachu_lightning.gif')";
+    pokemonOne.style.backgroundSize = '100% 100%';
+  });
 
-    var mewtwo = document.getElementById('mewtwo-ball');
-    mewtwo.addEventListener('click', function() {
-      pokemonTwo.style.backgroundImage = "url('images/mewtwo_fire.gif')"
-      pokemonTwo.style.backgroundSize = '100% 100%';
-    });
+  var mewtwo = document.getElementById('mewtwo-ball');
+  mewtwo.addEventListener('click', function() {
+    pokemonTwo.style.backgroundImage = "url('images/mewtwo_fire.gif')"
+    pokemonTwo.style.backgroundSize = '100% 100%';
+  });
 
-    var rapidash = document.getElementById('rapidash-ball');
-    rapidash.addEventListener('click', function() {
-      pokemonThree.style.backgroundImage = "url('images/rapidash.gif')"
-      pokemonThree.style.backgroundSize = '100% 100%';
-    });
+  var rapidash = document.getElementById('rapidash-ball');
+  rapidash.addEventListener('click', function() {
+    pokemonThree.style.backgroundImage = "url('images/rapidash.gif')"
+    pokemonThree.style.backgroundSize = '100% 100%';
+  });
 });
 
-document.getElementById('stephanie').addEventListener('click',function() {
-    loadPokemon('stephanie', 'type-null');
-    loadPokemon('stephanie', 'palkia');
-    loadPokemon('stephanie', 'barbaracle');
+var trainerStephanie = document.getElementById('stephanie');
 
-    pokemonOne.style.backgroundColor = '#4fd7f4';
-    pokemonTwo.style.backgroundColor = '#4fd7f4';
-    pokemonThree.style.backgroundColor = '#4fd7f4';
+trainerStephanie.addEventListener('click', function() {
+  loadPokemon('stephanie', 'type-null');
+  loadPokemon('stephanie', 'palkia');
+  loadPokemon('stephanie', 'barbaracle');
 
-    var typeNull = document.getElementById('type-null-ball');
-    typeNull.addEventListener('click', function() {
-      pokemonOne.style.backgroundImage = "url('images/type-null.gif')"
-      pokemonOne.style.backgroundSize = '100% 100%';
-    });
+  pokemonOne.style.backgroundColor = '#4fd7f4';
+  pokemonTwo.style.backgroundColor = '#4fd7f4';
+  pokemonThree.style.backgroundColor = '#4fd7f4';
 
-    var palkia = document.getElementById('palkia-ball');
-    palkia.addEventListener('click', function() {
-      pokemonTwo.style.backgroundImage = "url('images/palkia.gif')";
-      pokemonTwo.style.backgroundSize = '100% 100%';
-    });
+  trainerStephanie.classList.remove("blur");
+  infoGrid[1].classList.remove("hidden");
+  trainerAsh.classList.add("blur");
+  infoGrid[2].classList.add("hidden");
+  trainerRed.classList.add("blur");
+  infoGrid[0].classList.add("hidden");
 
-    var barbaracle = document.getElementById('barbaracle-ball');
-    barbaracle.addEventListener('click', function() {
-      pokemonThree.style.backgroundImage = "url('images/barbaracle.gif')";
-      pokemonThree.style.backgroundSize = '100% 100%';
-    });
+  var typeNull = document.getElementById('type-null-ball');
+  typeNull.addEventListener('click', function() {
+    pokemonOne.style.backgroundImage = "url('images/type-null.gif')"
+    pokemonOne.style.backgroundSize = '100% 100%';
+  });
+
+  var palkia = document.getElementById('palkia-ball');
+  palkia.addEventListener('click', function() {
+    pokemonTwo.style.backgroundImage = "url('images/palkia.gif')";
+    pokemonTwo.style.backgroundSize = '100% 100%';
+  });
+
+  var barbaracle = document.getElementById('barbaracle-ball');
+  barbaracle.addEventListener('click', function() {
+    pokemonThree.style.backgroundImage = "url('images/barbaracle.gif')";
+    pokemonThree.style.backgroundSize = '100% 100%';
+  });
 });
 
-document.getElementById('ashCatchNone').addEventListener('click',function() {
-    loadPokemon('ash', 'jolteon');
-    loadPokemon('ash', 'primeape');
-    loadPokemon('ash', 'scizor');
+var trainerAsh = document.getElementById('ashCatchNone');
 
-    pokemonOne.style.backgroundColor = '#00e68a';
-    pokemonTwo.style.backgroundColor = '#00e68a';
-    pokemonThree.style.backgroundColor = '#00e68a';
+trainerAsh.addEventListener('click', function() {
+  loadPokemon('ash', 'jolteon');
+  loadPokemon('ash', 'primeape');
+  loadPokemon('ash', 'scizor');
 
-    var jolteon = document.getElementById('jolteon-ball');
-    jolteon.addEventListener('click', function() {
-      pokemonOne.style.backgroundImage = "url('images/jolteon_shock.gif')"
-      pokemonOne.style.backgroundSize = '100% 100%';
-    });
+  pokemonOne.style.backgroundColor = '#00e68a';
+  pokemonTwo.style.backgroundColor = '#00e68a';
+  pokemonThree.style.backgroundColor = '#00e68a';
 
-    var primeape = document.getElementById('primeape-ball');
-    primeape.addEventListener('click', function() {
-      pokemonTwo.style.backgroundImage = "url('images/primeape_punching.gif')";
-      pokemonTwo.style.backgroundSize = '100% 100%';
-    });
+  trainerAsh.classList.remove("blur");
+  infoGrid[2].classList.remove("hidden");
+  trainerStephanie.classList.add("blur");
+  infoGrid[1].classList.add("hidden");
+  trainerRed.classList.add("blur");
+  infoGrid[0].classList.add("hidden");
 
-    var scizor = document.getElementById('scizor-ball');
-    scizor.addEventListener('click', function() {
-      pokemonThree.style.backgroundImage = "url('images/scizor.gif')";
-      pokemonThree.style.backgroundSize = '100% 100%';
-    });
+  var jolteon = document.getElementById('jolteon-ball');
+  jolteon.addEventListener('click', function() {
+    pokemonOne.style.backgroundImage = "url('images/jolteon_shock.gif')"
+    pokemonOne.style.backgroundSize = '100% 100%';
+  });
+
+  var primeape = document.getElementById('primeape-ball');
+  primeape.addEventListener('click', function() {
+    pokemonTwo.style.backgroundImage = "url('images/primeape_punching.gif')";
+    pokemonTwo.style.backgroundSize = '100% 100%';
+  });
+
+  var scizor = document.getElementById('scizor-ball');
+  scizor.addEventListener('click', function() {
+    pokemonThree.style.backgroundImage = "url('images/scizor.gif')";
+    pokemonThree.style.backgroundSize = '100% 100%';
+  });
 });
 
 function loadPokemon(trainer, pokemonName) {
@@ -136,29 +158,25 @@ function loadPokemon(trainer, pokemonName) {
         pokemon.abilities.push(abilityArray[obj]['ability']['name']);
       }
 
-      p_arr = [pokemon.name, 'HP: ' + pokemon.hp, 'Attack: '+ pokemon.atk, 'Defense: ' +pokemon.def, 'Abilities: ' +pokemon.abilities];
+      p_arr = [pokemon.name, 'HP: ' + pokemon.hp, 'Attack: ' + pokemon.atk, 'Defense: ' + pokemon.def, 'Abilities: ' + pokemon.abilities];
 
       console.log(abilityArray)
 
 
-      if(pokemonName == "pikachu" || pokemonName == "jolteon" || pokemonName =="type-null"){
-        for (obj in p_arr){
+      if (pokemonName == "pikachu" || pokemonName == "jolteon" || pokemonName == "type-null") {
+        for (obj in p_arr) {
           var stats = document.getElementsByClassName('pokemon1-stats')
           stats[obj].innerHTML = p_arr[obj];
 
         }
-      }
-
-      else if (pokemonName == "mewtwo" || pokemonName == "primeape" || pokemonName =="palkia") {
-        for (obj in p_arr){
+      } else if (pokemonName == "mewtwo" || pokemonName == "primeape" || pokemonName == "palkia") {
+        for (obj in p_arr) {
           var stats = document.getElementsByClassName('pokemon2-stats')
           stats[obj].innerHTML = p_arr[obj];
 
         }
-      }
-
-      else if(pokemonName == "rapidash" || pokemonName == "scizor" || pokemonName =="barbaracle"){
-        for (obj in p_arr){
+      } else if (pokemonName == "rapidash" || pokemonName == "scizor" || pokemonName == "barbaracle") {
+        for (obj in p_arr) {
           var stats = document.getElementsByClassName('pokemon3-stats')
           stats[obj].innerHTML = p_arr[obj];
 
@@ -172,9 +190,9 @@ function loadPokemon(trainer, pokemonName) {
 
   if (trainer == 'red' && redsPokemon.length !== 3) {
     redsPokemon.push(pokemon);
-  } else if(trainer == 'stephanie' && stephaniesPokemon.length !== 3) {
+  } else if (trainer == 'stephanie' && stephaniesPokemon.length !== 3) {
     stephaniesPokemon.push(pokemon);
-  } else if(trainer == 'ash' && ashsPokemon.length !== 3) {
+  } else if (trainer == 'ash' && ashsPokemon.length !== 3) {
     ashsPokemon.push(pokemon);
   }
 
